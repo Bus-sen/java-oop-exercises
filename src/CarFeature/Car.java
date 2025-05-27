c30d569b304e402e16bedb11f516452214c884bc
Bus-sen <snbusra66@gmail.com>
2025-05-27 15:48:17 +0300

    [JA-2] - constructor method was used

--- src/CarFeature/Car.java
+++ src/CarFeature/Car.java
@@ -4,4 +4,11 @@ public class Car {
     public String brand;
     public String model;
     public int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
 }
--- src/CarFeature/CarObject.java
+++ src/CarFeature/CarObject.java
@@ -3,6 +3,8 @@ package CarFeature;
 public class CarObject {
     public static void main(String[] args) {
 
+        /*
+        [JA-1]
         Car car = new Car();
         car.brand="Renault";
         car.model="Clio";
@@ -11,5 +13,11 @@ public class CarObject {
         System.out.println("Car Brand: " + car.brand);
         System.out.println("Car Model: " + car.model);
         System.out.println("Car Year: " + car.year);
+         */
+
+        Car car = new Car("Hyundai","Accent",1999);
+        System.out.println("Car Brand: "+ car.brand);
+        System.out.println("Car Model: "+ car.model);
+        System.out.println("Car Year: "+ car.year);
     }
 }
