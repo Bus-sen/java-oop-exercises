2e6bd031d8c774b918bd7cf4986c6fc48cb2187e
Bus-sen <snbusra66@gmail.com>
2025-05-30 12:45:11 +0300

    [JA-8] - static method was used

--- src/CarFeature/Vehicle.java
+++ src/CarFeature/Vehicle.java
@@ -6,4 +6,8 @@ public class Vehicle {
     public Vehicle(int speed) {
         this.speed = speed;
     }
+
+    public static int Multi(int number1, int number2){
+        return number1*number2;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import CarFeature.Vehicle;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Sonuç: " + Vehicle.Multi(5,9));
     }
 }
