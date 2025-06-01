b5dd69d31f0b61e3996cc92e4755d8967f6d763d
Bus-sen <snbusra66@gmail.com>
2025-06-01 13:28:32 +0300

    [JA-6] - polymorphism was used

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@ import CarFeature.Vehicle;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        [JA-8]
         System.out.println("Sonuç: " + Vehicle.Multi(5,9));
+         */
+
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+
+        cat.makeSound();
+        dog.makeSound();
     }
 }
